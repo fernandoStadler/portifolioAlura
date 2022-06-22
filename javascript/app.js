f640402ecfa40f865d2768b8cfbfaf7a6b17b5b3
Fernando Stadler <fernandostadler2@gmail.com>
2022-06-22 09:32:09 -0300

    Configuração envio de e-mail

--- javascript/app.js
+++ javascript/app.js
@@ -1,18 +1,36 @@
-let btnSend  = document.getElementById("submit")
+let btnSend = document.getElementById("submit")
 let subject = document.getElementById("subject");
 let inputName = document.getElementById("name");
 let mail = document.getElementById("mail");
 let message = document.getElementById("message");
+let responseMail = document.getElementById("response-mail");
+let textMsg = document.getElementById("text-msg");
 
-function sendEmail(){
-    emailjs.send("site.FernandoStadler","template_lzaqcbs",{
-        subject: subject.value,
-        name: inputName.value,
-        mail: mail.value,
-        message: message.value,
-        });
 
-    console.log(result)
+let templateParams = {
+    subject: subject.value,
+    name: inputName.value,
+    mail: mail.value,
+    message: message.value,
+};
+
+function cleanResponse(){
+    responseMail.className = "d-block"
+    textMsg.innerText = "";
+
+}
+function sendResponse(CssClassName, message) {
+    responseMail.className = CssClassName;
+    textMsg.innerText = message;
+    setInterval(cleanResponse, 10000);
+}
+
+function sendEmail() {
+    emailjs.send("site.FernandoStadler", "template_lzaqcbs", templateParams).then(function (response) {
+        sendResponse("send-sucess","O seu email foi enviado com sucesso!")
+    }, function () {
+        sendResponse("send-error","Email não enviado")
+    });
 }
 
 btnSend.onclick = sendEmail;
